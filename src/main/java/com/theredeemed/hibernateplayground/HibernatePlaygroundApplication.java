492ea543accd6acff42b28499fe6b2a90d22b58a
TheRedeemed <abdoulkaders77@gmail.com>
2020-08-02 16:33:20 -0400

    First commit

---
+++ src/main/java/com/theredeemed/hibernateplayground/HibernatePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.theredeemed.hibernateplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatePlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatePlaygroundApplication.class, args);
+	}
+
+}
